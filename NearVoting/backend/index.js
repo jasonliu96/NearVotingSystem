6c5d8d2d9b99f1b84ac68da2fd234f3848d06430
jasonliu96 <fmbxjason@gmail.com>
2022-04-27 08:39:57 -0700

    typo

--- NearVoting/backend/index.js
+++ NearVoting/backend/index.js
@@ -76,7 +76,7 @@ async function run(){
     })
     let counter =0; 
     app.post('/addCandidate', async (req,res) => {
-        ({text} = req);
+        ({text} = req).body;
         try {
             const result = await contract.addCandidate({args:{'text':text}});
             res.json(
