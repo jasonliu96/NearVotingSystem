e8fc1f7d0c7623de22bcae28b0c3b23ebf3dd303
Jason <fmbxjason@gmail.com>
2022-05-13 10:53:02 -0700

    simple result page

--- NearVoting/src/components/User/ResultsPage.jsx
+++ NearVoting/src/components/User/ResultsPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-// import { PieChart } from 'react-minimal-pie-chart';
+import { PieChart } from 'react-minimal-pie-chart';
+import { Card,List, ListItemText} from '@mui/material';
 
 function ResultsPage() {
   const [numVotes, setNumVotes] = React.useState(0)
   const [candidates, setCandidates] = React.useState([])
+  const [selected, setSelected] = React.useState(0);
+  const [data, setData] = React.useState([])
+  const colors = ['#ebf5fb','#d6eaf8','#aed6f1','#85c1e9','#5dade2','#3498db','#b0bec5','#90a4ae','#78909c','#607d8b','#546e7a', '#455a64']
+  function mapCandidates(candidates){
+    const temp = candidates.filter((value, index) => (value.votes>0)).map((value, index)=>({title:value.name, value:value.votes, color:colors[index%10]}))
+    setData(temp)
+  }
   React.useEffect(
     () => {
       // in this case, we only care to query the contract when signed in
@@ -17,6 +25,7 @@ function ResultsPage() {
         window.contract.getCandidates({  })
         .then(candidatesFromContract => {
           setCandidates(candidatesFromContract)
+          mapCandidates(candidatesFromContract)
         })
       }
     },
@@ -26,12 +35,32 @@ function ResultsPage() {
     // This works because signing into NEAR Wallet reloads the page
     []
   )
+
   return (
-    <div>
+    <>
+    <main>
       <h1>Results</h1>
-      <h2>Total Number of Votes: {numVotes}</h2>
-      
-    </div>
+      <div style={{display:'flex', flexDirection:'row'}}>
+        <div style={{padding:10, width:'60%', height:'60%'}}>
+        <PieChart
+        animate
+        data={data}
+        style={{
+          fontSize: '5px',
+        }}
+        lineWidth={80}
+        label={({ dataEntry }) => (dataEntry.title)}
+      />
+      </div>
+      <Card sx={{border: 1, p: 1, m: 1, maxWidth: 'auto', maxHeight:'auto', borderColor:'#ebf5fb'}}>
+      <List>
+        <ListItemText>Total Votes: {numVotes}</ListItemText>
+        {data.map((value, index)=> <ListItemText>{value.title +': '+value.value}</ListItemText>)}
+      </List>
+      </Card>
+      </div>
+    </main>
+    </>
   );
 }
 
