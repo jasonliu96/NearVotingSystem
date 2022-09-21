fe8319be58bd782d264f8a41a4c2a7db4186fc01
Jason <fmbxjason@gmail.com>
2022-09-20 17:48:21 -0700

    Updates to candidate functionality
    Stores mongodb object ad in the blockchain now
    
    candidate information retrieval handled in backend

--- NearVoting/backend/routes/candidate/candidateRoutes.js
+++ NearVoting/backend/routes/candidate/candidateRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const { addCandidate, checkUniquecandidateId } = require('../../queries/candidate/candidate')
+const { addCandidate, checkUniquecandidateId, getCandidateInfo } = require('../../queries/candidate/candidate')
 
 router.post('/addCandidate', async (req, res) => {
   try {
@@ -9,8 +9,8 @@ router.post('/addCandidate', async (req, res) => {
 
     let results = null
     results = await addCandidate(req.body, res)
-    console.log(`routes results ${results.data} and ${results.status}`)
-    res.send(results.status, results.data)
+    // console.log(`routes results ${results.data} and ${results.status}`)
+    res.send(results.status, {"msg":results.data, "id":results.id})
   } catch (err) {
     console.log(`Catch error: ${err}`)
     return err
@@ -33,4 +33,15 @@ router.post('/checkUniquecandidateId', async (req, res) => {
   }
 })
 
+router.post('/getCandidateInfo', async (req, res) => {
+  try{
+    console.log(`getCandidateInfo req body: ${JSON.stringify(req.body)}`)
+    results = await getCandidateInfo(req.body, res)
+    console.log(results.data)
+    res.send(200, results.data)
+  } catch(err) {
+    console.log(`error from getCandidateInfo: ${err}`)
+    return err
+  }
+})
 module.exports = router
