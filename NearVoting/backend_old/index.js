61c8359b28b86e9ac16afbe8dc3ea63c32613ed0
Jason <fmbxjason@gmail.com>
2022-09-29 18:59:37 -0700

    Update index.js

--- NearVoting/backend_old/index.js
+++ NearVoting/backend_old/index.js
@@ -129,6 +129,7 @@ async function run(){
         }
     })
     app.post('/addCandidateCompressed', async (req,res) => {
+        const {text} = req;
         const compStr = LZUTF8.compress(text, {outputEncoding:"StorageBinaryString"})
         try {
             const result = await contract.addCandidate({args:{'text':compStr}});
